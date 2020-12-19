64f07befb9f792fef851002ba8762e1a60aa367c
Thiago da Silveira Souza <thiagodasilveirasouza@gmail.com>
2020-12-19 14:17:54 -0300

    Carousel imgs
    update carousel imgs

--- src/components/carousel-component/service/data/carouselComponentData.js
+++ src/components/carousel-component/service/data/carouselComponentData.js
@@ -2,7 +2,7 @@ export const carouselComponentData = [
   {
     id: 1,
     name: "foto-1",
-    pathName: "./assets/carousel-component/foto-1.png",
+    pathName: "./assets/carousel-component/foto-1.jpg",
   },
   {
     id: 2,
@@ -12,11 +12,11 @@ export const carouselComponentData = [
   {
     id: 3,
     name: "foto-3",
-    pathName: "./assets/carousel-component/foto-3.jpeg",
+    pathName: "./assets/carousel-component/foto-3.jpg",
   },
   {
     id: 4,
     name: "foto-4",
-    pathName: "./assets/carousel-component/sobre-nos-img.jpg",
+    pathName: "./assets/carousel-component/foto-4.jpg",
   },
 ]
